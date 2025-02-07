69fe38f0d8b72982d8288568105d49e72eec9659
Patricia IsabelBenitez Ramos <brpisabel@gmail.com>
2025-02-06 22:06:09 -0600

    creación de array, lista para nombres

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear las variables que usare.
+let ListaDeAmigosParaSorteo = [];//aqui agrego el array que guarda los nombres
+let NumeroMaximoSorteo = 10; 
+
+
+
+function agregarAmigo(nombre) {
+    if (!nombre) {
+        console.log("Nombre vacío no se puede agregar")
+        return;
+    }
+    ListaDeAmigosParaSorteo.push(nombre);
+    console.log(`✅${nombre} hasido agregado ya a esta lista.`);
+}
